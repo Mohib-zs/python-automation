86624b39c01833f14590ce7dc2878327dcdd1472
Mohib Shaikh <mohibzahid97@gmail.com>
2024-09-29 21:16:49 +0500

    Completed website monitoring module

--- monitor-website.py
+++ monitor-website.py
@@ -2,6 +2,7 @@ import smtplib
 import requests 
 import os
 import paramiko
+import schedule
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.compute import ComputeManagementClient
 from email.mime.text import MIMEText
@@ -32,42 +33,46 @@ def send_email(subject, message):
 
 
 def restart_container():
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname="mohibzahid.eastus.cloudapp.azure.com", username="azureuser", key_filename="C:/Users/mohib/.ssh/id_rsa")
-    stdin, stdout, stderr = ssh.exec_command('docker start 0358ccc9930f')
-    print(stdout.readlines())
-    ssh.close()
-    print("Application restarted")
+  ssh = paramiko.SSHClient()
+  ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  ssh.connect(hostname="mohibzahid.eastus.cloudapp.azure.com", username="azureuser", key_filename="C:/Users/mohib/.ssh/id_rsa")
+  stdin, stdout, stderr = ssh.exec_command('docker start 0358ccc9930f')
+  print(stdout.readlines())
+  ssh.close()
+  print("Application restarted")
 
+def monitor_website():
+  try:
+    # Send a GET request to the health check URL
+    response = requests.get(url_to_check)
 
-try:
-  # Send a GET request to the health check URL
-  response = requests.get(url_to_check)
+    if response.status_code == 200:
+      # Application is running
+      print("Application running successfully")
+      message = MIMEText(f"Application is running! Response code: {response.status_code}")        #Email message
+      message['Subject'] = "Application Status: Running"                                          #Email Subject
+      send_email(message['Subject'], message)
+    else:
+      # Application is not running
+      print("Application not running")
+      message = MIMEText(f"Application is not running! Response code: {response.status_code}")
+      message['Subject'] = "Application Status: Not Running"
+      send_email(message['Subject'], message)
+      restart_container()
 
-  if response.status_code == 200:
-    # Application is running
-    print("Application running successfully")
-    message = MIMEText(f"Application is running! Response code: {response.status_code}")        #Email message
-    message['Subject'] = "Application Status: Running"                                          #Email Subject
+  except Exception as e:
+    # Error occurred such as connection failure
+    print("Error Occured")
+    message = MIMEText(f"An error occurred: {str(e)}")
+    message['Subject'] = "Application Status: Error"
     send_email(message['Subject'], message)
-  else:
-    # Application is not running
-    print("Application not running")
-    message = MIMEText(f"Application is not running! Response code: {response.status_code}")
-    message['Subject'] = "Application Status: Not Running"
-    send_email(message['Subject'], message)
-    restart_container()
 
-except Exception as e:
-  # Error occurred such as connection failure
-  print("Error Occured")
-  message = MIMEText(f"An error occurred: {str(e)}")
-  message['Subject'] = "Application Status: Error"
-  send_email(message['Subject'], message)
-
-  print("Rebooting server")
-  compute_client.virtual_machines.begin_restart(resource_group_name, vm_name)
-  restart_container()                 #You can also use 'docker update --restart unless-stopped <container-id>' cmd on server to automate container restart when server restart
+    print("Rebooting server")
+    compute_client.virtual_machines.begin_restart(resource_group_name, vm_name)
+    restart_container()                 #You can also use 'docker update --restart unless-stopped <container-id>' cmd on server to automate container restart when server restart
 
 
+schedule.every(5).minutes.do(monitor_website)
+
+while True:
+  schedule.run_pending()
